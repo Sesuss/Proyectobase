5d136ebd48fca33040803a7675e967ddf8980c75
Sesuss <83632004+Sesuss@users.noreply.github.com>
2021-11-28 22:40:34 -0600

    Update pdf.js

--- src/routes/pdf.js
+++ src/routes/pdf.js
@@ -2,17 +2,17 @@ const puppeteer = require("puppeteer")
 
 
 async function crearfacturas(url){
-
+console.log("Entro segunda funcion")
     let navegador=await puppeteer.launch()
-
+console.log("1")
     let pagina = await navegador.newPage()
-
+console.log("2")
     await pagina.goto(url)
-
+console.log("3")
     let pdf=await pagina.pdf()
-
+console.log("4")
     navegador.close()
-
+console.log("5")
     return pdf
 }
 
@@ -20,12 +20,15 @@ async function crearfacturas(url){
 module.exports={
 
     factura(req,res){
+        console.log("Entro a la pagina de la factura")
         res.render("factura.hbs",{ layout:"mainpdf"})
+        console.log("Salio a la pagina de la factura")
     },
 
     async descargar(req,res){
+        console.log("Entro")
         let pdf=await crearfacturas("https://ejemplo-name.herokuapp.com/ver")
-
+        console.log("Salio")
         res.contentType("application/pdf")
         res.send(pdf)
 
